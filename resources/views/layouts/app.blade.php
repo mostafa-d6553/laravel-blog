6686e2296ace8fa41e29dbf911b956045075eb18
mostafa <you@example.com>
2021-09-05 16:37:17 +0430

    change style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,31 +7,68 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>وبلاگ {{ $title }}</title>
     
-    <link rel="stylesheet" href="{{ ('/blog/css/style.css') }}">
-    <link rel="stylesheet" href="{{ ('/blog/css/responsive.css') }}" media="(max-width:991px)">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
     <!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.4.1/css/swiper.min.css">-->
 </head>
 <body>
-
+<div class="bg-dark mb-3 pb-5 text-white">
 <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-        <span class="fs-4">Simple header</span>
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom ">
+      <a href="{{ route('landing') }}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <h2>Blog</h2>
       </a>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-      </ul>
+      <div class="text-end">
+          <a href="{{ route('login') }}"><button type="button" class="btn btn-outline-light me-2">Login</button></a>
+          <a href="{{ route('register') }}"><button type="button" class="btn btn-warning">Register</button></a>
+      </div>
     </header>
   </div>
 
+  
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark d-flex justify-content-center py-3">
+  <div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a class="nav-link active text-white" aria-current="page" href="{{ route('landing') }}">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Programming
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Hack
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>
+
+</div>
 {{ $slot }}
 
 <div class="overlay"></div>
